0428ec81208db7810a4039420acaa1c3f4909099
Boris Spektor <boris@soom.la>
2015-08-11 11:56:22 +0300

    Added version

--- src/com/soomla/store/billing/google/GooglePlayIabService.java
+++ src/com/soomla/store/billing/google/GooglePlayIabService.java
@@ -48,6 +48,8 @@ import java.util.Map;
  */
 public class GooglePlayIabService implements IIabService {
 
+    public static final String VERSION = "1.0.3";
+
     public GooglePlayIabService() {
         configVerifyPurchases(null);    // we reset it every run
     }
