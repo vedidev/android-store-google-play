8973313f29de037c29e59d0b3bb90cfd41ff7245
Boris Spektor <boris@soom.la>
2015-08-16 20:48:37 +0300

    Rebuild jar with correct verification url

--- src/com/soomla/store/billing/google/GooglePlayIabService.java
+++ src/com/soomla/store/billing/google/GooglePlayIabService.java
@@ -48,7 +48,7 @@ import java.util.Map;
  */
 public class GooglePlayIabService implements IIabService {
 
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
 
     public GooglePlayIabService() {
         configVerifyPurchases(null);    // we reset it every run
